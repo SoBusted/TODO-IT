da3e267f352e6f0abc625447b3d0140fde7430bb
SoBusted <fireintheholl@naver.com>
2020-06-13 14:57:15 +0900

    Add clearAll button

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,12 +1,29 @@
 <template lang="pug">
-    div footer
+    .clearAllContainer
+        span.clearAllBtn(@click="clearTodo") Clear All
 </template>
 
 <script>
 export default {
-
+    methods: {
+        clearTodo() {
+            localStorage.clear();
+        }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+    .clearAllContainer {
+        width: 8.5rem;
+        height: 50px;
+        line-height: 50px;
+        background-color: white;
+        border-radius: 5px;
+        margin: 0 auto;
+    }
+    .clearAllBtn {
+        display: block;
+        color: #e20303;
+    }
 </style>
