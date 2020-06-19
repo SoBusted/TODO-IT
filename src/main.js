8c61651092c208bb27b8b55e5c4cbbcfe5ef603a
SoBusted <fireintheholl@naver.com>
2020-06-20 00:04:37 +0900

    Add vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
 import App from './App';
 import store from './store';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify);
 
 new Vue({
   render: h => h(App),
+  vuetify: new Vuetify(),
   store
 }).$mount('#app');
